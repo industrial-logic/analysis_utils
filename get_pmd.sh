f9262625a7c97e12b9338d179303a1931b0062e3
Brett L. Schuchert <brett.schuchert@gmail.com>
2020-04-06 13:09:27 -0500

    Script to get pmd and extract into script dir

---
+++ get_pmd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+function latest_pmd {
+	curl  --silent https://github.com/pmd/pmd/releases/latest | sed "s/.*https/https/" | sed "s/tag/download/" | sed 's/\([0-9.]*\)".*/\1\/pmd-bin-\1.zip/'
+}
+
+
+
+PMD_DIR=$(find $SCRIPT_DIR -maxdepth 1 -type d -name "pmd*")
+if [ -z $PMD_DIR ]; then
+	pushd $SCRIPT_DIR > /dev/null
+	curl --silent -L -O $(latest_pmd)
+	unzip -qq pmd*.zip 
+	rm pmd*.zip
+	popd > /dev/null
+fi
