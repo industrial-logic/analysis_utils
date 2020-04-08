5d654fbb9e043c8cd11f95845a627afb358bdd19
Brett L. Schuchert <brett.schuchert@gmail.com>
2020-04-08 15:19:22 -0500

    Using git_utils.sh in dup.sh

--- dup.sh
+++ dup.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 . $SCRIPT_DIR/get_pmd.sh 
+. $SCRIPT_DIR/git_utils.sh
 
 FILE_LIST=./filelist
 FILE_PATTERN="[.]java"
@@ -18,11 +19,13 @@ function files_matching_pattern {
 function review {
 	commit=$1
 	pattern=$2
-	file_count=$(files_matching_pattern $commit $pattern)
-	if [ $file_count -gt 0 ]
-	then
-   		cat $FILE_LIST
-		$SCRIPT_DIR/pmd-bin-*/bin/run.sh cpd --minimum-tokens 25 --filelist $FILE_LIST 
+
+  git_files_matching_pattern $commit $pattern >$FILE_LIST
+  cat $FILE_LIST
+  file_count=$(cat $FILE_LIST | wc -l)
+
+	if [ $file_count -gt 0 ]; then
+		$SCRIPT_DIR/pmd-bin-*/bin/run.sh cpd --minimum-tokens 25 --filelist $FILE_LIST
 	else
 		echo "No files matching '$2'"
 	fi
@@ -30,21 +33,16 @@ function review {
 
 
 function walk {
-   file_pattern=$1
+  file_pattern=$1
 	git rev-list HEAD | while read commit
 	do
 		echo '-------------------------------------------'
 		echo $commit
-		git checkout $commit
+		git checkout $commit > /dev/null 2>&1
 		review $commit $file_pattern
 	done
 }
 
-function restore {
-	git checkout .
-	git checkout master &> /dev/null
-	rm filelist
-}
-
+git_restore
 walk $FILE_PATTERN
-restore
+git_restore
