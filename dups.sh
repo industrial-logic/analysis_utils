7fea73731fb7dd5962565be2cb4b0bb627c0f1d7
Brett L. Schuchert <bschuch2@ford.com>
2020-04-15 14:37:45 -0500

    Fixed error in previous commit that I had already pushed

--- dups.sh
+++ dups.sh
@@ -5,7 +5,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 . "${SCRIPT_DIR}/bashlibs/get_pmd.sh" $SCRIPT_DIR
-. "${SCRIPT_DIR}/bashlibs/check_code.sh" $SCRIPT_DIR
+. "${SCRIPT_DIR}/bashlibs/check_args.sh" $SCRIPT_DIR
 . "${SCRIPT_DIR}/bashlibs/execute_cpd.sh" $SCRIPT_DIR
 
 check_code
