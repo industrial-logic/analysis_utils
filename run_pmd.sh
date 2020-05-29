8e2e40600d49b4f42eb723e7148bd1e44b6188e1
Brett L. Schuchert <brett.schuchert@gmail.com>
2020-05-29 16:47:36 -0500

    run pmd on code

---
+++ run_pmd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# shellcheck source=.
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+. "${SCRIPT_DIR}/bashlibs/banner.sh" $SCRIPT_DIR
+. "${SCRIPT_DIR}/bashlibs/self_update.sh" $SCRIPT_DIR
+. "${SCRIPT_DIR}/bashlibs/get_pmd.sh" $SCRIPT_DIR
+. "${SCRIPT_DIR}/bashlibs/check_args.sh" $SCRIPT_DIR
+. "${SCRIPT_DIR}/bashlibs/execute_pmd.sh" $SCRIPT_DIR
+
+print_banner
+self_update
+check_code
