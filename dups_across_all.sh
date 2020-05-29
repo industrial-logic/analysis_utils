8a4876ef076b45bdede40572050ce2073dfd2c30
Brett L. Schuchert <bschuch2@ford.com>
2020-05-29 09:28:27 -0500

    Changing this to find all duplication

--- dups_across_all.sh
+++ dups_across_all.sh
@@ -4,12 +4,12 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-find . -name src  | while read dir; do 
+find . -name src  | while read dir; do
 	project_dir=`dirname $dir | cut -d/ -f2`
 	echo "|||||$project_dir {"
 	pushd $project_dir > /dev/null
-   git pull >/dev/null 2>&1
-	$SCRIPT_DIR/dups_in_this_commit.sh
+    git pull >/dev/null 2>&1
+	$SCRIPT_DIR/dups.sh
 	echo "} $project_dir"
 	popd > /dev/null
 done
