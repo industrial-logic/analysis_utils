39a1cfa71c6e96ded28178f74d951d8029469c09
Brett L. Schuchert <bschuch2@ford.com>
2020-07-02 12:57:27 -0500

    Added commit date to output

--- last_commit_dups.sh
+++ last_commit_dups.sh
@@ -17,12 +17,13 @@ PROJ=`basename $PWD`
 
 for i in $(git log --pretty=format:"%H" -n $TOTAL_COMMITS_BACK);do
     git checkout $i 2>/dev/null
+    COMMIT_DATE=$(git show -s --format=%ci $i)
     START_COMMIT=$(git log --pretty=format:'%h' -n 1)
     START_TOKEN_COUNT=$(total)
     git checkout HEAD^ 2>/dev/null
     END_TOKEN_COUNT=$(total)
     let DIFF=`expr $START_TOKEN_COUNT - $END_TOKEN_COUNT`
-    printf "%-30s: Diff: %8d Current: %7d Previous: %7d Commit: %7s\n" $PROJ $DIFF $START_TOKEN_COUNT $END_TOKEN_COUNT $START_COMMIT
+    printf "%-30s: Diff: %8d Current: %7d Previous: %7d Commit: %7s [%s]\n" "$PROJ" "$DIFF" "$START_TOKEN_COUNT" "$END_TOKEN_COUNT" "$START_COMMIT" "$COMMIT_DATE"
 done
 
 git checkout master >/dev/null 2>&1
