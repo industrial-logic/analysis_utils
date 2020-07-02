163072b7cec1d01d9448acea23efaf6fa28c6742
Brett L. Schuchert <bschuch2@ford.com>
2020-07-02 11:20:14 -0500

    Can now go back a number of commits

--- last_commit_dups.sh
+++ last_commit_dups.sh
@@ -2,15 +2,27 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+TOTAL_COMMITS_BACK=1
+
+if [ "$#" -eq 1  ]; then
+    TOTAL_COMMITS_BACK=$1
+fi
+
 function total {
     echo $($SCRIPT_DIR/dups.sh 2>/dev/null | $SCRIPT_DIR/utils/summarize.awk | awk -F"," 'BEGIN{sum=0} {sum += $5} END {print sum}')
 }
 
-START_TOKEN_COUNT=$(total)
-git checkout HEAD^ 2>/dev/null
-END_TOKEN_COUNT=$(total)
-git checkout master >/dev/null 2>&1
-let DIFF=`expr $START_TOKEN_COUNT - $END_TOKEN_COUNT`
 PWD=`pwd`
 PROJ=`basename $PWD`
-printf "%-30s: Diff: %4d Current: %6d Previous: %6d\n" $PROJ $DIFF $START_TOKEN_COUNT $END_TOKEN_COUNT
+
+for i in $(git log --pretty=format:"%H" -n $TOTAL_COMMITS_BACK);do
+    git checkout $i 2>/dev/null
+    START_COMMIT=$(git log --pretty=format:'%h' -n 1)
+    START_TOKEN_COUNT=$(total)
+    git checkout HEAD^ 2>/dev/null
+    END_TOKEN_COUNT=$(total)
+    let DIFF=`expr $START_TOKEN_COUNT - $END_TOKEN_COUNT`
+    printf "%-30s: Diff: %8d Current: %7d Previous: %7d Commit: %7s\n" $PROJ $DIFF $START_TOKEN_COUNT $END_TOKEN_COUNT $START_COMMIT
+done
+
+git checkout master >/dev/null 2>&1
