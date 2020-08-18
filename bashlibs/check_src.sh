4e0a16f9908cae0ad63d47cfde3ac4b1e498a9ed
Brett L. Schuchert <bschuch2@ford.com>
2020-08-18 09:25:06 -0500

    Updated to use [[

--- bashlibs/check_src.sh
+++ bashlibs/check_src.sh
@@ -2,7 +2,7 @@
 
 function check_src_dir() {
 
-    if [ ! $(find . -type d -name src) ]; then
+    if [[ ! $(find . -type d -name src) ]]; then
 		echo "There doesn't seem to be a ./src directory."
 		echo "Did you run this script in a maven/gradle based Java project?"
 		exit 1
