f9911c6438ae73a2b03d73c880e75da306d23a9d
Brett L. Schuchert <bschuch2@ford.com>
2020-08-18 09:22:40 -0500

    Updated plot to show past 4 weeks only

--- utils/dups_by_commit.sh
+++ utils/dups_by_commit.sh
@@ -12,7 +12,7 @@ function total {
     echo $($SCRIPT_DIR/../dups.sh 2>/dev/null | $SCRIPT_DIR/summarize.awk | awk -F"," 'BEGIN{sum=0} {sum += $2} END {print sum}')
 }
 
-for i in $(git log --pretty=format:'%H' --since=8.weeks); do
+for i in $(git log --pretty=format:'%H' --since=4.weeks); do
     git checkout $i 2>/dev/null
     COMMIT_DATE=$(git show -s --format=%ci --date=local $i)
     COMMIT_DATE=$(gdate +"%Y-%m-%d %H:%M:%S" -d "$COMMIT_DATE")
