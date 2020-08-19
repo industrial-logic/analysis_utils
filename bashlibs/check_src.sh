b1064fbe7c31864f477f9ae2f386448162eb1a71
Brett L. Schuchert <bschuch2@ford.com>
2020-08-19 09:42:34 -0500

    Using check_src instead of hand-coded

--- bashlibs/check_src.sh
+++ bashlibs/check_src.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 function check_src_dir() {
-
     if [[ ! $(find . -type d -name src) ]]; then
 		echo "There doesn't seem to be a ./src directory."
 		echo "Did you run this script in a maven/gradle based Java project?"
--- plot_dups.sh
+++ plot_dups.sh
@@ -2,9 +2,8 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-if [ ! -d "./src" ]; then
-    echo "Does not appear to be a java project" >&2
-    exit 1
-fi
+. "${SCRIPT_DIR}/bashlibs/check_src.sh"
+
+check_src_dir
 
 $SCRIPT_DIR/utils/dups_by_commit.sh | $SCRIPT_DIR/utils/to_gnuplot.sh | gnuplot
