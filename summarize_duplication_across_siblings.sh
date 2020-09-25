885ee129460615745193875e15391f46ba00d387
Brett L. Schuchert <bschuch2@ford.com>
2020-09-25 10:54:28 -0500

    Added arg checking to a few of the functions

--- summarize_duplication_across_siblings.sh
+++ summarize_duplication_across_siblings.sh
@@ -2,16 +2,22 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+. "${SCRIPT_DIR}/bashlibs/check_args.sh" $SCRIPT_DIR
+
 function line() {
     printf "%6s, %-30s, %s\n" "$1" "$2" "$3"
 }
 
 function countDuplication() {
+	  check_args 1 "$@"
+
     DIR=$1
     (cd $DIR; $SCRIPT_DIR/summarize.sh 2>/dev/null | awk -F"," 'BEGIN{sum=0} {sum+=$2} END {print sum}')
 }
 
 function firstCommitDate() {
+	  check_args 1 "$@"
+
     DIR=$1
     (cd $DIR; git log --reverse --format=%ci | head -1)
 }
