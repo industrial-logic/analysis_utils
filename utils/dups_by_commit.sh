034d9cb45bd920abc69d9665fa0a3de1c0363f7c
Brett L. Schuchert <bschuch2@ford.com>
2020-10-01 10:52:11 -0500

    Update so that a minimum of 2 commits are counted, to allow at least a line in the plot

--- utils/dups_by_commit.sh
+++ utils/dups_by_commit.sh
@@ -12,12 +12,29 @@ function total {
     "$SCRIPT_DIR"/../dups.sh 2>/dev/null | "$SCRIPT_DIR"/summarize.awk | "$SCRIPT_DIR"/total_lines.awk
 }
 
-BACK=${1:-"4.weeks"}
-
-for i in $(git log --pretty=format:'%H' --since="$BACK"); do
-    git checkout $i 2>/dev/null
-    COMMIT_DATE=$(git show -s --format=%ci --date=local $i)
+function reportOnCommit {
+    COMMIT_DATE=$(git show -s --format=%ci --date=local)
     COMMIT_DATE=$(gdate +"%Y-%m-%d %H:%M:%S" -d "$COMMIT_DATE")
     LINES=$(total)
     printf "%s,%d\n" "$COMMIT_DATE" "$LINES"
+}
+
+BACK=${1:-"4.weeks"}
+
+COMMITS_SO_FAR=0
+
+for i in $(git log --pretty=format:'%H' --since="$BACK"); do
+    git checkout $i 2>/dev/null
+    reportOnCommit
+    COMMITS_SO_FAR=$((COMMITS_SO_FAR+1))
+    LAST_COMMIT="$i"
+done
+
+LAST_COMMIT="${LAST_COMMIT:-HEAD}"
+
+while [ $COMMITS_SO_FAR -lt 2 ]; do
+    git checkout "$LAST_COMMIT"^ 2>/dev/null
+    reportOnCommit
+    COMMITS_SO_FAR=$((COMMITS_SO_FAR+1))
+    LAST_COMMIT="$LAST_COMMIT"^
 done
