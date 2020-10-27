6dbe1e70049093e92fbbe3bb43f01a1a1d128165
Mike Vitale <github@mikevitale.com>
2020-10-27 07:42:34 -0500

    tabs/spaces

--- dups_across_all.sh
+++ dups_across_all.sh
@@ -12,9 +12,8 @@ find . -name src  | while read dir; do
 	project_dir=`dirname $dir | cut -d/ -f2`
 	echo "|||||$project_dir {"
 	pushd "$project_dir" > /dev/null || exit
-  git pull >/dev/null 2>&1
+	git pull >/dev/null 2>&1
 	"$SCRIPT_DIR"/dups.sh
 	echo "} $project_dir"
 	popd > /dev/null || exit
 done
-
