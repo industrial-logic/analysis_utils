3d1592721339d722bffd1a12d7c28f241b14aa9c
Mike Vitale <github@mikevitale.com>
2021-03-05 10:31:04 -0600

    Updated run_pmd.sh to have a default language of Java; it's expected to be the second command-line argument.  Also updated execute_pmd.sh to have a '-L' on the 'find' command to work with symlink directories.  Language is passed from run_pmd.sh to execute_pmd.sh.

--- bashlibs/execute_pmd.sh
+++ bashlibs/execute_pmd.sh
@@ -3,17 +3,18 @@
 # shellcheck source=.
 
 script_dir="$1"
+LANGUAGE=$3
 
 . "${script_dir}/bashlibs/get_pmd.sh"
 . "${script_dir}/bashlibs/check_src.sh"
 
 RULES="${script_dir}/rulesets/${2}"
-RUN_PMD=$(find "$script_dir" -name run.sh)
+RUN_PMD=$(find -L "$script_dir" -name run.sh)
+
 function check_code() {
     FILELIST=$(mktemp)
     find . -type d -name src > "$FILELIST"
-	check_src_dir
-    "$RUN_PMD" pmd -filelist "$FILELIST" -rulesets "$RULES" -f text 2>/dev/null
+    check_src_dir
+    "$RUN_PMD" pmd -language $LANGUAGE -filelist $FILELIST -rulesets $RULES -f text 2>/dev/null
     rm "$FILELIST"
 }
-
--- run_pmd.sh
+++ run_pmd.sh
@@ -2,12 +2,14 @@
 
 # shellcheck source=.
 
+LANGUAGE=${2:-'java'}
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 . "${SCRIPT_DIR}/bashlibs/banner.sh" $SCRIPT_DIR
 . "${SCRIPT_DIR}/bashlibs/get_pmd.sh" $SCRIPT_DIR
 . "${SCRIPT_DIR}/bashlibs/check_args.sh" $SCRIPT_DIR
-. "${SCRIPT_DIR}/bashlibs/execute_pmd.sh" $SCRIPT_DIR $1
+. "${SCRIPT_DIR}/bashlibs/execute_pmd.sh" $SCRIPT_DIR $1 $LANGUAGE
 
 print_banner
 check_code
